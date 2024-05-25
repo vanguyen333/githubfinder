383d150fb3aeb058a783788145bef730e93cdf48
vanguyen333 <vanguyen333@yahoo.com>
2024-05-25 13:55:28 -0400

    #48 Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import { Home, About, NotFound } from './pages';
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from './Home.jsx';
+import About from './About.jsx';
+import NotFound from './NotFound.jsx';
+
+export { Home, About, NotFound };
