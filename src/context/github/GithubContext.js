f5e291ab679dd343c618f768e5223021e2e21dff
vanguyen333 <vanguyen333@yahoo.com>
2024-05-26 19:19:53 -0400

    #58 Clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -34,11 +34,19 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
   // Set loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
