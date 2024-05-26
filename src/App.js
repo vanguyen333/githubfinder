f0836dc46bb880b517bf41344529912215eeb365
vanguyen333 <vanguyen333@yahoo.com>
2024-05-26 19:53:48 -0400

    #60 Alert Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/layout/Footer';
 import { Home, About, NotFound } from './pages';
 import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
+import Alert from './components/layout/Alert';
 function App() {
   return (
     <GithubProvider>
@@ -12,6 +13,7 @@ function App() {
           <div className="flex flex-col justify-between h-screen">
             <Navbar />
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
